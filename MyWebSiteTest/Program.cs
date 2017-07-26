ac227fe144b0803437718156061d198045f12c35
stagebo <1254117589@qq.com>
2017-07-26 21:19:56 +0800

    完成NHibernate配置。2017年7月26日21:19:54

--- MyWebSiteTest/Program.cs
+++ MyWebSiteTest/Program.cs
@@ -13,7 +13,14 @@ namespace MyWebSiteTest
     {
         static void Main(string[] args)
         {
-            ISession v=SessionManager.OpenSession();
+            string sql = "select * from t_message";
+            ISession session=SessionManager.OpenSession();
+            session = SessionManager.OpenSession();
+            //加载SQL语句
+            ISQLQuery iSQLQuery = session.CreateSQLQuery(sql);
+
+            //查询结果并返回
+            var t= iSQLQuery.List<object>().ToList<object>();
             int[] a = { 1,2,3};
             string s = string.Join("#",a);
             Console.WriteLine(s);
