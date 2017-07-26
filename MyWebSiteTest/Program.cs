3ac7af8a3e6d8edb9c70c114d4a239417d47c0a9
stagebo <1254117589@qq.com>
2017-07-26 16:03:40 +0800

    tt。2017年7月26日16:03:38

--- MyWebSiteTest/Program.cs
+++ MyWebSiteTest/Program.cs
@@ -10,6 +10,10 @@ namespace MyWebSiteTest
     {
         static void Main(string[] args)
         {
+            int[] a = { 1,2,3};
+            string s = string.Join("#",a);
+            Console.WriteLine(s);
+            Console.ReadKey(); ;
         }
     }
 }
