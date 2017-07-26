eb68fdf2b4a052ea96447263bdf55c5bb92bb1a1
stagebo <1254117589@qq.com>
2017-07-26 21:40:06 +0800

    完成基本流程。2017年7月26日21:40:04

--- MyWebSit/App_Start/RouteConfig.cs
+++ MyWebSit/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyWebSit
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MyWebSit/Controllers/Common/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Common.Log4Net;
+using Common.NHibernate;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebSit.Controllers.Common
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Page_Login");
+        }
+        public ActionResult Test()
+        {
+            string sql = "select * from t_message";
+            ISession session = SessionManager.OpenSession();
+            session = SessionManager.OpenSession();
+            //加载SQL语句
+            ISQLQuery iSQLQuery = session.CreateSQLQuery(sql);
+
+            //查询结果并返回
+            var t = iSQLQuery.List<object>().ToList<object>();
+            int[] a = { 1, 2, 3 };
+            Log4NetUtils.Error(this,"errr");
+            string s = string.Join("#", a);
+            return null;
+        }
+    }
+}
