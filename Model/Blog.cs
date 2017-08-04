e0d849d3a0000acf464738427999b73934ffb6ba
stagebo <1254117589@qq.com>
2017-08-04 16:51:25 +0800

    新建博客框架，内容TODO。2017年8月4日16:51:22

---
+++ Model/Blog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Base;
+
+namespace Model
+{
+    class Blog:BaseModel
+    {
+
+        public override string ToJsonString(bool flag)
+        {
+            return base.ToJsonString();
+        }
+    }
+}
+//USE[BlogSystem]
+//GO
+
+//CREATE TABLE[dbo].[t_blog](
+//	[f_id]
+//    [uniqueidentifier]
+//    NOT NULL,
+
+//    [f_title] [nvarchar](256) NOT NULL,
+
+//[f_second_title] [nvarchar](512) NULL,
+//	[f_content]
+//    [text]
+//    NULL,
+//	[f_state]
+//    [int]
+//    NOT NULL,
+
+//    [f_create_time] [datetime]
+//    NOT NULL,
+
+//    [f_modify_time] [datetime]
+//    NOT NULL,
+
+//    [f_writer_id] [uniqueidentifier]
+//    NOT NULL,
+
+//    [f_type] [int]
+//    NULL,
+//	[f_exist]
+//    [int]
+//    NOT NULL,
+// CONSTRAINT[PK_t_blog] PRIMARY KEY CLUSTERED
+//(
+//   [f_id] ASC
+//)WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON[PRIMARY]
+//) ON[PRIMARY] TEXTIMAGE_ON[PRIMARY]
+
+//GO
---
+++ MyWebSit/Controllers/Blog/BlogController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebSit.Controllers.Blog
+{
+    public class BlogController : Controller
+    {
+        /// <summary>
+        /// GET /Blog/Index
+        /// </summary>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Page_Search_Blog_List");
+        }
+        /// <summary>
+        /// GET /Blog/AddBlogPage
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddBlogPage()
+        {
+            return View("Page_Add_Blog");
+        }
+        /// <summary>
+        /// GET /Blog/BlogManagePage 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult BlogManagePage()
+        {
+            return View("Page_Blog_Manage");
+        }
+
+
+    }
+}
